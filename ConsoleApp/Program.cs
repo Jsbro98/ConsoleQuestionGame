68273c9d2c9ae3b4a32e550ef6bdb4fb507123f8
Jsbro98 <Jsbro98@github.com>
2023-05-03 03:43:36 -0500

    added new while loop to check feeling, adjusted name while loop

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -36,6 +36,11 @@ namespace ConsoleApp
             Console.WriteLine();
             Console.WriteLine("Please pick a color.");
 
+            string colorChoice;
+            string feelingChoice;
+            string representsChoice;
+            string favoriteColorChoice;
+
             while(true)
             {
                 foreach (Color color in colorArray)
@@ -43,16 +48,63 @@ namespace ConsoleApp
                     Console.Write(color.Name + " ");
                 }
 
-                string? colorChoice = Console.ReadLine();
+                Console.WriteLine();
 
-                if (colorChoice is not null)
+                colorChoice = Console.ReadLine()!;
+
+                if ( !String.IsNullOrEmpty(colorChoice) )
                 {
                     colorChoice = colorChoice.ToLower();
-                    break;
+                    colorChoice = Char.ToUpper(colorChoice[0]) + colorChoice.Substring(1);
+                    
+                    if (colorChoice != red.Name && colorChoice != blue.Name && colorChoice != yellow.Name)
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine("Please choose a valid color.");
+                    }
+                    else
+                    {
+                        break;
+                    }
                 }
                 else
                 {
                     Console.WriteLine("Please choose a color.");
+                }
+            }
+
+            while (true)
+            {
+                Console.WriteLine();
+                Console.WriteLine("What feeling does the color have? Please choose from the choices below");
+
+                foreach (Color color in colorArray)
+                {
+                    Console.Write(color.Feeling + " ");
+                }
+
+                Console.WriteLine();
+
+                feelingChoice = Console.ReadLine()!;
+
+                if ( !String.IsNullOrEmpty(feelingChoice) )
+                {
+                    feelingChoice = feelingChoice.ToLower();
+                    feelingChoice = Char.ToUpper(feelingChoice[0]) + feelingChoice.Substring(1);
+
+                    if (feelingChoice != red.Feeling && feelingChoice != blue.Feeling && feelingChoice != yellow.Feeling)
+                    {
+                        Console.WriteLine("Please choose a feeling from the options listed.");
+                        feelingChoice = "";
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Please enter a feeling.");
                 }
             }
         }
